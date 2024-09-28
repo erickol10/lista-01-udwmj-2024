56e764d64864f4c5c237375fe943762830eb61fd
Erick Oliveira <129996694+erickol10@users.noreply.github.com>
2024-09-28 00:37:37 -0300

    Calculadora Adição Subtração

---
+++ Calculadora Adição Subtração.js
@@ -0,0 +1,14 @@
+function adicionar(num1, num2) {
+    return num1 + num2;
+}
+
+function subtrair(num1, num2) {
+    return num1 - num2;
+}
+
+// Exemplo de uso
+let numero1 = 5;
+let numero2 = 3;
+
+console.log("Adição: " + adicionar(numero1, numero2)); // Saída: Adição: 8
+console.log("Subtração: " + subtrair(numero1, numero2)); // Saída: Subtração: 2
